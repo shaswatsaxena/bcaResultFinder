d78393afcb2eb97192899fb4c99049d995cced43
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-01-25 22:25:07 +0530

    Linting

---
+++ eslintrc.js
@@ -0,0 +1,98 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ["react-app", "airbnb", "prettier", "prettier/react"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  plugins: ["prettier", "react", "react-hooks"],
+  rules: {
+    "no-return-assign": ["error", "except-parens"],
+    "no-restricted-syntax": [
+      2,
+      "ForInStatement",
+      "LabeledStatement",
+      "WithStatement"
+    ],
+    "no-unused-vars": [
+      1,
+      {
+        ignoreSiblings: true,
+        argsIgnorePattern: "res|next|^err"
+      }
+    ],
+    "prefer-const": [
+      "error",
+      {
+        destructuring: "all"
+      }
+    ],
+    "arrow-body-style": [2, "as-needed"],
+    "no-unused-expressions": [
+      2,
+      {
+        allowTaggedTemplates: true
+      }
+    ],
+    "no-param-reassign": [
+      2,
+      {
+        props: false
+      }
+    ],
+    "no-console": 1,
+    "import/prefer-default-export": 0,
+    import: 0,
+    "func-names": 0,
+    "max-len": 0,
+    "import/extensions": 0,
+    "consistent-return": 0,
+    "react/display-name": 1,
+    "react/no-array-index-key": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/prefer-stateless-function": 0,
+    "react/forbid-prop-types": 0,
+    "react/no-unescaped-entities": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/accessible-emoji": 0,
+    "react/require-default-props": 0,
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".jsx"]
+      }
+    ],
+    radix: 0,
+    "no-shadow": [
+      2,
+      {
+        hoist: "all",
+        allow: ["resolve", "reject", "done", "next", "err", "error"]
+      }
+    ],
+    quotes: [
+      2,
+      "single",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "all",
+        singleQuote: true,
+        printWidth: 80
+      }
+    ]
+  }
+};
