5f642d523778585e4b42fc8179ae62438b8269a9
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-19 13:46:29 +0530

    all results are now statically generated using fallback

--- src/pages/result/[year]/[rollNumber]/[semester].js
+++ src/pages/result/[year]/[rollNumber]/[semester].js
@@ -1,10 +1,24 @@
+import { useRouter } from "next/router";
+
+// importing components
 import Result from "../../../../components/Result";
 
 const ResultData = ({ data }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading....</div>;
+  }
   return <div>{<Result {...data} />}</div>;
 };
 
-export const getServerSideProps = (context) => {
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps = (context) => {
   const ctx = context.params;
   const data = {
     year: ctx.year,
